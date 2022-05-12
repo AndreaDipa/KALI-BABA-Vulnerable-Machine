68ee400e128e1161cec91872232c1ae07fffc8b2
AndreaDipa <andreadipaa@gmail.com>
2022-05-12 05:16:07 -0400

    trying pug

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express")
 
 const app = express()
 app.disable('x-powered-by');
+app.set('view engine', 'pug')
+
 app.get("/", (req, res) => {
-    res.send("Hello Worl")
-})
+    res.render('index', { title: 'PND AGAINST THR MACHNE', message: 'spognardi infame' })})
 
 app.listen(3000, '127.0.0.1', () => {
     console.log("app listening on port 3000")
