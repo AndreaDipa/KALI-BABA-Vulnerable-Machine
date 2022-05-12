a450b818e6c67369b1093ef7892abb9a07f7ea85
AndreaDipa <andreadipaa@gmail.com>
2022-05-12 15:54:20 -0400

    remove pug

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express")
-
+const bodyParser = require('body-parser')
 const app = express()
-app.disable('x-powered-by');
-app.set('view engine', 'pug')
+app.disable('x-powered-by')
+app.use( bodyParser.json() ) 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 
 app.get("/", (req, res) => {
-    res.render('index', { title: 'PND AGAINST THR MACHNE', message: 'spognardi infame' })})
+    res.send("Hello World")
+})
 
 app.listen(3000, '127.0.0.1', () => {
     console.log("app listening on port 3000")
