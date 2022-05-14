b8943d48b19ecdbd37403742fe811446dc9a73bc
AndreaDipa <andreadipaa@gmail.com>
2022-05-14 09:50:35 -0400

    rce

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const bodyParser = require('body-parser')
+const fileupload = require("express-fileupload");
+const http = require('http')
 const app = express()
 app.disable('x-powered-by')
 app.use( bodyParser.json() ) 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(fileupload({ parseNested: true }));
 
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.render('index')
 })
 
 app.listen(3000, '127.0.0.1', () => {
