eaa373d1a1de1562e58135960bfafc1014cdb8e0
lucarep <81578112+lucarep@users.noreply.github.com>
2022-05-24 22:14:43 +0200

    git buggato

---
+++ views/JS/fileExtension.js
@@ -0,0 +1,10 @@
+let input = document.getElementById("form-check").value;
+let extension = input.split('.').pop()
+let modal = document.getElementById("mymodal")
+const formats = ["png","jpg","jpeg"];
+
+() => {
+    /*if (!formats.includes(extension)) {
+        modal.style.display = "block";
+    }*/
+}
